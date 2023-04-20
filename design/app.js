25986890f58aed5233927871dc95a1070956efe0
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-04-20 14:38:54 -0400

    app.js Update

--- design/app.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelector(".main__col-2-input");
-const label = document.querySelector(".main__col-2-placeholder");
-
-input.addEventListener("click", function() {
-  label.style.display = "none";
-});
