da3e0cd4e01f83a33316fd142143790b0f8cd394
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-04-23 16:22:47 -0400

    Update app.js

--- design/app.js
+++ design/app.js
@@ -1,61 +1,60 @@
-// Get submit element
-const submit = document.querySelector('.main__col-2-submit');
+const submitBtn = document.querySelector('.main__col-2-submit');
 
-// Get input fields
-const firstNameInput = document.querySelector('#input-field-1');
-const lastNameInput = document.querySelector('#input-field-2');
-const emailInput = document.querySelector('#input-field-3');
-const passwordInput = document.querySelector('#input-field-4');
-
-// Get error elements
-const firstNameError = document.querySelector('.main__col-2-input-box:nth-of-type(1) .main__col-2-error');
-const lastNameError = document.querySelector('.main__col-2-input-box:nth-of-type(2) .main__col-2-error');
-const emailError = document.querySelector('.main__col-2-input-box:nth-of-type(3) .main__col-2-error');
-const passwordError = document.querySelector('.main__col-2-input-box:nth-of-type(4) .main__col-2-error');
-
-
-// Add submit event listener to submit
-submit.addEventListener('click', (event) => {
-  // Validate first name
-  if (firstNameInput.value === '') {
-    firstNameError.innerHTML = 'First name is cannot be empty';
-    firstNameInput.style.border = '2px solid var(--clr-primary-red)'
-    
-    event.preventDefault();
-  } else {
-    firstNameError.innerHTML = '';
+const formInputs = [
+  { 
+    inputField: document.querySelector('#input-field-1'),
+    errorField: document.querySelector('.main__col-2-input-box:nth-of-type(1) .main__col-2-error'),
+    iconField: document.querySelector('.main__col-2-input-box:nth-of-type(1) .main__col-2-icon')
+  },
+  { 
+    inputField: document.querySelector('#input-field-2'),
+    errorField: document.querySelector('.main__col-2-input-box:nth-of-type(2) .main__col-2-error'),
+    iconField: document.querySelector('.main__col-2-input-box:nth-of-type(2) .main__col-2-icon')
+  },
+  {
+    inputField: document.querySelector('#input-field-3'),
+    errorField: document.querySelector('.main__col-2-input-box:nth-of-type(3) .main__col-2-error'),
+    iconField: document.querySelector('.main__col-2-input-box:nth-of-type(3) .main__col-2-icon')
+  },
+  {
+    inputField: document.querySelector('#input-field-4'),
+    errorField: document.querySelector('.main__col-2-input-box:nth-of-type(4) .main__col-2-error'),
+    iconField: document.querySelector('.main__col-2-input-box:nth-of-type(4) .main__col-2-icon')
   }
+];
 
-  // Validate last name
-  if (lastNameInput.value === '') {
-    lastNameError.innerHTML = 'First name is cannot be empty';
-    lastNameInput.style.border = '2px solid var(--clr-primary-red)'
-    
+function validateInputs(inputs) {
+  let hasError = false;
+  inputs.forEach(
+    (
+      { inputField, errorField, iconField }
+    ) => {
+    if (inputField.value === '') {
+      errorField.innerHTML = `${inputField.name} cannot be empty`;
+      inputField.style.border = '2px solid var(--clr-primary-red)';
+      iconField.classList.toggle('active');
+      hasError = true;
+    } else if (inputField.type === 'email' && !inputField.validity.valid) {
+      errorField.textContent = 'Please enter a valid email address';
+      inputField.style.border = '2px solid var(--clr-primary-red)';
+      iconField.classList.toggle('active');
+      hasError = true;
+    } else {
+      errorField.textContent = '';
+    }
+  });
+  return !hasError;
+}
 
-    event.preventDefault();
-  } else {
-    lastNameError.innerHTML = '';
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (validateInputs(formInputs)) {
+    event.target.form.submit();
   }
-
-  // Validate email
-  if (emailInput.value === '') {
-    emailError.innerHTML = "Email is cannot be empty";
-    emailInput.style.border = '2px solid var(--clr-primary-red)'
-  } else if (!emailInput.validity.valid) {
-    emailError.innerHTML = 'Please enter a valid email address';
-    emailInput.style.border = '2px solid var(--clr-primary-red)'
-    event.preventDefault();
-  } else {
-    emailError.innerHTML = '';
-  }
-
-  // Validate password
-  if (passwordInput.value === '') {
-    passwordError.innerHTML = 'Password is cannot be empty';
-    passwordInput.style.border = '2px solid var(--clr-primary-red)'
-    event.preventDefault();
-    
-  } else {
-    passwordError.innerHTML = '';
-  }
+  
+  formInputs.forEach(input => {
+    if (input.inputField.type === 'email') {
+      input.inputField.style.color = 'var(--clr-primary-red)';
+    }
+  });
 });
